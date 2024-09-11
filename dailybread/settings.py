3eb4de78b81202b504e782098cc03e9be7da017e
lo-franky <lo.frankyl@gmail.com>
2024-09-11 15:57:13 -0500

    adjusting deployment configurations

--- dailybread/settings.py
+++ dailybread/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('jb9Gj78vholklTRv')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not 'ON_HEROKU' in os.environ:
